67c9f09cbc9c822f18802a11d670c4bd1874e707
Diego Barros <diegobarros2812@gmail.com>
2020-02-02 19:39:04 -0400

    Adding commented example to database.js

---
+++ templates/project/config/database.js
@@ -0,0 +1,12 @@
+// const knex = require('knex')({
+//   client: 'pg',
+//   connection: {
+//     host: 'localhost',
+//     user: 'username',
+//     password: 'password',
+//     database: 'database'
+//   }
+// });
+
+
+// module.exports = knex;
