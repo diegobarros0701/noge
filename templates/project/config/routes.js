712ee996f75c96e9e6ecd26cb96fae9c752aef4e
Diego Barros <diegobarros2812@gmail.com>
2020-02-02 19:38:50 -0400

    Commenting code in routes.js to be as an example

--- templates/project/config/routes.js
+++ templates/project/config/routes.js
@@ -1,7 +1,7 @@
-const router = require('express').Router();
+// const router = require('express').Router();
 
-router.get('/users', UserController.index);
-router.get('/users/:id', UserController.show);
-router.post('/users', UserController.create);
-router.patch('/users/:id', UserController.patch);
-router.delete('/users/:id', UserController.destroy);
+// router.get('/users', UserController.index);
+// router.get('/users/:id', UserController.show);
+// router.post('/users', UserController.create);
+// router.patch('/users/:id', UserController.patch);
+// router.delete('/users/:id', UserController.destroy);
