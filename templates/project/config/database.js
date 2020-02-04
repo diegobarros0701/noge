cca23f51624a61d52d189548153d3775ebc9d1a9
Diego Barros <diegobarros2812@gmail.com>
2020-02-03 21:52:05 -0400

    Updating database file config

--- templates/project/config/database.js
+++ templates/project/config/database.js
@@ -1,12 +1,13 @@
-// const knex = require('knex')({
-//   client: 'pg',
-//   connection: {
-//     host: 'localhost',
-//     user: 'username',
-//     password: 'password',
-//     database: 'database'
-//   }
-// });
+const knex = require('knex')({
+  client: 'pg',
+  debug: process.env.DEBUG,
+  connection: {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+  }
+});
 
-
-// module.exports = knex;
+module.exports = knex;
