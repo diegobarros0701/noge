8154190db5953f465cde977a6547329c1bca5abf
Diego Barros <diegobarros2812@gmail.com>
2020-02-05 20:54:54 -0400

    Changing controller, model and service command to accepts an array to generate

--- bin/index.js
+++ bin/index.js
@@ -29,43 +29,53 @@ program.
 program
   .command('scaffold <name> [actions...]')
   .description('create a model, controller and route')
-  .option('--no-spec', 'do not generate spec file for generated model')
   .option('--empty-controller', 'generate a controller without default actions. This overrides the actions option')
+  .option('--no-spec', 'do not generate spec file for generated model')
   .option('-t, --table [name]', 'the corresponding table name of the model')
   .action(function (name, actions, options) {
     ScaffoldGenerator.go({ name, actions, options });
   });
 
 program
-  .command('controller <name> [actions...]')
+  .command('controller <names...>')
   .description('create a controller')
-  .option('--no-spec', 'do not generate spec file for generated controller')
-  .option('--empty-controller', 'generate a controller without default actions. This overrides the actions option')
+  .option('--actions <actions>', 'specify wich actions to generate: Available values are: index, show, create, update and destroy')
   .option('--async-await-style', 'generate controller actions using async await instead of promises')
-  .action(async function (controllerName, actions, options) {
-    ControllerGenerator.go({ name: controllerName, options, actions })
-      .catch(error => console.log(error.message));
+  .option('--empty', 'generate a controller without default actions. This overrides the actions option')
+  .option('--no-spec', 'do not generate spec file for generated controller')
+  .action(async function (controllersNames, options) {
+
+    controllersNames.forEach(function (controllerName) {
+      ControllerGenerator.go({ name: controllerName, options })
+        .catch(error => console.log(error.message));
+    });
   });
 
 program
-  .command('model <name>')
+  .command('model <names...>')
   .description('create a model')
   .option('--no-spec', 'do not generate spec file for generated model')
   .option('-t, --table [name]', 'the corresponding table name of the model')
-  .action(function (modelName, options) {
-    ModelGenerator.go({ name: modelName, options })
-      .catch(error => console.log(error.message));
+  .action(function (modelsNames, options) {
+
+    modelsNames.forEach(function (modelName) {
+      ModelGenerator.go({ name: modelName, options })
+        .catch(error => console.log(error.message));
+    });
   });
 
 program
-  .command('service <name>')
+  .command('service <names...>')
   .description('create a service')
   .option('--actions <actions>', 'specify which actions to generate for the service')
+  .option('--empty', 'do not generate actions for the service')
   .option('--project-path <path>', 'the path for project dir if it is different from the current dir')
-  .option('--empty-body', 'do not generate actions for the service')
-  .action(function (serviceName, options) {
-    ServiceGenerator.go({ serviceName, options })
-      .catch(error => console.log(error.message));
+  .action(function (servicesNames, options) {
+
+    servicesNames.forEach(function (serviceName) {
+      ServiceGenerator.go({ serviceName, options })
+        .catch(error => console.log(error.message));
+    });
   });
 
 program.parse(process.argv);
